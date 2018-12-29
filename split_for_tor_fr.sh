37db6f47f5e6ffdabe9742ea43257f76b4d149b4
Jidor Tang <tlqtangok@126.com>
2018-12-29 18:07:26 +0800

    - edit split_for_tor_fr.sh at 20181229_1807

--- split_for_tor_fr.sh
+++ split_for_tor_fr.sh
@@ -1,7 +1,7 @@
 # split -b 1m large_file.txt
 
 # tor 
-ls xa* | perl -e ' print q(export tor="perl $perl_p/tor.PL").qq(\n\n);  map{ chomp; print qq(\$tor $_ &\n); }<>;'
+ls xa* | perl -e ' print q(export tor="perl $perl_p/tor.PL").qq(\n\n);  map{ chomp; print qq(\$tor $_ &\n); }<>;print qq(wait\necho end\n)'
 
 # fr
 perl -e ' print q(export fr="perl $perl_p/fr.PL").qq(\n\n);  map{ chomp; print qq(\$fr jd_$_ &\n); }(191..197); print qq(wait\necho end\n)'
