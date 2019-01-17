09af745384a17d937de8e08927150efe705c8a9d
Jidor Tang <tlqtangok@126.com>
2019-01-17 16:44:02 +0800

    parallel update tor , fr

---
+++ update_tor_fr.sh
@@ -0,0 +1,13 @@
+# ● linux
+curl http://algoers.com:10241/tor_fr/linux/tor > tor &  
+curl http://algoers.com:10241/tor_fr/linux/fr > fr &
+wait 
+
+chmod 0777 tor fr
+
+#● pi
+# curl http://algoers.com:10241/tor_fr/pi/tor > tor && curl http://algoers.com:10241/tor_fr/pi/fr > fr
+
+# ● win
+#curl http://algoers.com:10241/tor_fr/win/tor.exe > tor.exe && curl http://algoers.com:10241/tor_fr/win/fr.exe > fr.exe
+
