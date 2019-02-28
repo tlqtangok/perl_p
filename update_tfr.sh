99160666f3794b5a0959ee1560a12094a3ce83a4
Jidor Tang <tlqtangok@126.com>
2019-02-28 09:58:32 +0800

    add or edit: update_tfr.sh at 20190228_0958

--- update_tfr.sh
+++ update_tfr.sh
@@ -1,13 +1,12 @@
 # ● linux
-curl http://algoers.com:10241/tor_fr/linux/tor > tor &  
-curl http://algoers.com:10241/tor_fr/linux/fr > fr &
+curl http://algoers.com:10241/tor_fr/linux/tfr > tfr &  
 wait 
 
 chmod 0777 tor fr
 
 #● pi
-# curl http://algoers.com:10241/tor_fr/pi/tor > tor && curl http://algoers.com:10241/tor_fr/pi/fr > fr
+# curl http://algoers.com:10241/tor_fr/pi/tfr > tfr 
 
 # ● win
-#curl http://algoers.com:10241/tor_fr/win/tor.exe > tor.exe && curl http://algoers.com:10241/tor_fr/win/fr.exe > fr.exe
+#curl http://algoers.com:10241/tor_fr/win/tfr.exe > tfr.exe
 
