2ae9f61dbae7f79baf893dffb1cef938c3e047bf
tlqtangok <tlqtangok@126.com>
2020-06-05 17:25:27 +0800

    add work in any root

--- cp_to_bak.sh
+++ cp_to_bak.sh
@@ -4,7 +4,9 @@ set -e
 for i in "$@"
 do
 #	echo $i
-FN_FULL_PATH=$i
+FN=$i
+FN_FULL_PATH=$(readlink -f $FN)
+
 basename_fn=$(basename $FN_FULL_PATH)
 dirname_fn=$(dirname $FN_FULL_PATH)
 cd $dirname_fn
