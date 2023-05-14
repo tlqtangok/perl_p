fdeab5c7407df6721d6405c4ca14622d4a2f7e2b
tlqtangok <tlqtangok@126.com>
2023-05-14 17:09:12 +0800

    add support for fsrc

--- fsrc.bat
+++ fsrc.bat
@@ -1,6 +1,7 @@
 @echo off 
-# can work on linux
+:: can work on linux
 set fn=%tmp%\fn_all.txt
-dir /s /b > %fn%
-perl %perl_p%\filter_src_fn.PL %fn% 
+dir /s /b > %fn% 
+dos2unix %fn% 2>nul 
+perl %perl_p%\filter_src_fn.PL %fn%  %1 %2 %3 %4 %5 %6 %7 %8
 @echo on
