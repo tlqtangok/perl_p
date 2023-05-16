1aadf23a4ed8539722927f95f0c7fc7ef30de574
tlqtangok <tlqtangok@126.com>
2023-05-16 10:04:04 +0800

    set_proxy

--- set_proxy.bat
+++ set_proxy.bat
@@ -2,4 +2,4 @@ set HTTPS_PROXY=http://127.0.0.1:7890
 set HTTP_PROXY=http://127.0.0.1:7890
 
 
-REM cannot use ping www.google.com to verify, only can use curl www.google.com
+REM  cannot use ping www.google.com to verify, only can use curl www.google.com
