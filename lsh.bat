40e5667fc1eab53a5059041294115cb878480dd0
tlqtangok <tlqtangok@126.com>
2023-05-23 10:55:39 +0800

    add win lshp with num

--- lsh.bat
+++ lsh.bat
@@ -1,9 +1,13 @@
 @echo off
 set this_dir=%cd%
 ::echo %this_dir%
+set last_few_num=7
 if not "%1" == "" (
-set this_dir=%1
+set last_few_num=%1
 )
 pushd %this_dir% > nul
-dir /b /o-D  |perl -e "@arr=<>; map{print ' '.@arr[$_];}(0..7);"
+
+dir /b /o-D  |perl -e "@arr=<>; chomp(@arr); @arr_sort = sort{-M $a <=> -M $b}@arr; map{print qq{ $_\n};}@arr_sort[0..$ENV{last_few_num}];" 
+
 popd
+
