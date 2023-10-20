e9a6af8a799afdcf5e5cb47b99d8828912092fd3
林奇 <tlqtangok@126.com>
2023-10-20 12:57:01 +0800

    modify time

--- lsh.bat
+++ lsh.bat
@@ -7,7 +7,7 @@ set last_few_num=%1
 )
 pushd %this_dir% > nul
 
-dir /b /o-D  |perl -e "@arr=<>; chomp(@arr); @arr_sort = sort{-M $a <=> -M $b}@arr; map{print qq{ $_\n};}@arr_sort[0..$ENV{last_few_num}];" 
+dir /b /o-D /TW |perl -e "@arr=<>; chomp(@arr); @arr_sort = sort{-M $a <=> -M $b}@arr; map{print qq{ $_\n};}@arr_sort[0..$ENV{last_few_num}];" 
 
 popd
 
