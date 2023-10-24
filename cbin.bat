e676111055c88c1168ca86b6a73dfaf7943df4ce
林奇 <tlqtangok@126.com>
2023-10-24 14:16:26 +0800

    Update cbin.bat

--- cbin.bat
+++ cbin.bat
@@ -1,8 +1,8 @@
 @echo off 
 pushd %0\..
-perl %perl_p%\cbin.PL %1 > %perl_p%\cbin_log.bat
+perl %perl_p%\cbin.PL %1 > %perl_p%\win_bin\cbin_log.bat
 ::type %perl_p%\cbin_log.bat
 popd 
-call %perl_p%\cbin_log.bat 
+call %perl_p%\win_bin\cbin_log.bat 
 
 @echo on 
