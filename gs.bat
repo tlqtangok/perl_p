1df0c337620e75337c4315418c554f5907a18e91
林奇 <tlqtangok@126.com>
2024-07-02 14:39:02 +0800

    gs to gstol , add condition

--- gs.bat
+++ gs.bat
@@ -1,3 +1,15 @@
 @echo off 
+
+
 git status -s 
+
+git status -s |grepw "^.M" > nul
+set ret_code=%ERRORLEVEL%
+
+perl -e "print qq(\n);"
+
+if %ret_code% equ 0 (
+    git status -s | grepw "^.M" | repl "^.M" "" |tol | repl "^" " git add  "
+)
+
 @echo on 
--- gstol.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-git status -s 
-perl -e "print qq(\n);"
-
-git status -s | grepw ".M" | repl "^.M" "" |tol | repl "^" " git add  "
-@echo on 
