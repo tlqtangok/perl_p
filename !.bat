6b88ceb2f761e94dd1269fda4cd8bb0024c5441f
林奇 <tlqtangok@126.com>
2024-09-09 08:37:26 +0800

    fix h first cml error

--- !.bat
+++ !.bat
@@ -6,10 +6,10 @@ FOR %%A IN (%*) DO SET /A pcnt+=1
 
 
 
+set e_his_cmd=%tmp%\e_his_cmd.bat
 set MYLINENO=%1
 if %pcnt% EQU 1 (
     perl %perl_p%\get_e_cml_from_his.PL  %MYLINENO%
-    set e_his_cmd=%tmp%\e_his_cmd.bat
     call %e_his_cmd%
 )
 
