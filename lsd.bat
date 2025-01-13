dffa154cf516fbba718dc1295e11e18064668f55
林奇 <tlqtangok@126.com>
2025-01-13 09:14:02 +0800

    add lsd.bat

--- lsd.bat
+++ lsd.bat
@@ -2,4 +2,7 @@
 
 for %%i in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (if exist %%i:\ echo %%i:)
 
+
+
+perl -e " my @fc = `net use|grepw OK`;   if (@fc){$_=$fc[0]; my @a=split m/\s+/; print qq(\n),$a[1] , qq( => ), $a[2],qq(\n);  }"; 
 @echo on 
