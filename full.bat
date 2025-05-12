8082575186dbcd8e5d5a5a7566003f19e7782222
林奇 <tlqtangok@126.com>
2025-05-12 14:50:37 +0800

    change ff full fullw

---
+++ full.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal enabledelayedexpansion
+
+::echo File Path Listing - %date% %time%
+::echo --------------------------------------
+
+if "%~1"=="" (
+    echo Error: No parameters provided
+    echo Usage: %~nx0 [file or folder] [file or folder] ...
+    goto :end
+)
+
+:process
+if "%~1"=="" goto :end
+
+set "item=%~1"
+set "item=%item:/=\%"
+
+if exist "%item%" (
+    for %%i in ("%item%") do echo  %%~fi
+) else (
+    echo   [ERROR] Path not found: %item%
+)
+
+shift
+goto :process
+
+:end
+::echo --------------------------------------
+::echo Processing complete
---
+++ fullw.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal enabledelayedexpansion
+
+::echo File Path Listing - %date% %time%
+::echo --------------------------------------
+
+if "%~1"=="" (
+    echo Error: No parameters provided
+    echo Usage: %~nx0 [file or folder] [file or folder] ...
+    goto :end
+)
+
+:process
+if "%~1"=="" goto :end
+
+set "item=%~1"
+set "item=%item:/=\%"
+if 0 == 1 (
+    if exist "%item%" (
+        for %%i in ("%item%") do echo  %%~fi
+    ) else (
+        echo   [ERROR] Path not found: %item%
+    )
+)
+
+if exist "%item%" (
+    for %%i in ("%item%") do (
+        set "unix_path=%%~fi"
+        set "unix_path=!unix_path:\=/!"
+        echo   !unix_path!
+    )
+) else (
+    echo   [ERROR] Path not found: %item%
+)
+
+shift
+goto :process
+
+:end
+::echo --------------------------------------
+::echo Processing complete
