549d8f2eead7772019db8969934cc1b4bb2f7963
林奇 <tlqtangok@126.com>
2025-06-03 15:10:27 +0800

    tarc with log

--- alias_win.cmd
+++ alias_win.cmd
@@ -2,7 +2,8 @@
 
 :: reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d "d:\jd\perl_p\alias_win.cmd" /f
 ::
-doskey tarc=tar czf $*
+::doskey tarc=tar czf $*
+doskey tarc=tar cvzf $* ^>%TEMP%\tarc.log 2^>^&1 ^&^& ^( ^( findstr /N "^" %TEMP%\tarc.log ^| findstr /B /L "11:" ^>NUL 2^>^&1 ^&^& ^( head -n 5 %TEMP%\tarc.log ^&^& echo ... ^&^& tail -n 5 %TEMP%\tarc.log ^) ^|^| ^( type %TEMP%\tarc.log ^) ^) ^| uniq ^)
 doskey tart=tar tvzf $*
 ::doskey tarx=tar xvzf $* > %tmp%\tarx.log 2>&1 &&  (head -n 5 %tmp%\tarx.log && echo ... && tail -n 5 %tmp%\tarx.log ) | uniq
 ::
