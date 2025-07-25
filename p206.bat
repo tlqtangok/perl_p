b46480bed545ed7898344e3c9120758c543c7815
林奇 <tlqtangok@126.com>
2025-07-25 16:29:53 +0800

    add p206

---
+++ p206.bat
@@ -0,0 +1,16 @@
+@echo off 
+::pushd \\192.168.16.206\软件安装包\预装软件
+
+lsd |grepw /I Z  > nul
+
+if %errorlevel% equ 0 (
+pushd z:\
+goto EOF_
+)
+
+set p206=\\192.168.16.206\smb_share_2T
+pushd %p206% > nul 
+
+:EOF_
+
+@echo on 
